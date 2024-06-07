a6326ec253d2b593c2e3e21140019f4b8ce62762
Nobin <143606368+NobinSijo360t@users.noreply.github.com>
2024-06-07 16:08:36 +0530

    Create Day 0.c

---
+++ Day 0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+   char inputString[105]; 
+   scanf("%[^\n]", inputString); 
+  
+  
+   printf("Hello, World.\n");
+printf("%s", inputString);
+
+   return 0;
+}
